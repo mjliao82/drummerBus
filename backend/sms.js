8f42e92f2940c828fdbc7a9481655c8349012d90
Lucas <laust@email.sc.edu>
2025-03-26 11:25:03 -0400

    twilio fix

--- backend/sms.js
+++ backend/sms.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./backend/.env" });
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
