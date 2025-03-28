e37e281d9a0e69f6cb4fa765534cc012924e3df2
chry4n <rchen72@buffalo.edu>
2025-03-27 20:12:40 -0400

    Emails are now sent from Anthony's email

--- backend/testEmail.js
+++ backend/testEmail.js
@@ -3,9 +3,9 @@ const sendEmail = require('./email'); // Import your email module
 
 // Test email
 sendEmail(
-    'rchen72@buffalo.edu',  // Change this to the recipient's email
+    'Anthony@lidrumbus.com',  // Change this to the recipient's email
     'Test Email',
-    'Hello!\n This is a test email for IQVentory' // Change message if needed
+    'Hello Anthony!\n This is a test email from IQVentory' // Change message if needed
 )
 .then(response => console.log(response))
 .catch(error => console.error(error));
